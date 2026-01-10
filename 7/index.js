580dc6292dbbd2b20e535cc874a58bc5d27c4bb2
Hiu11 <Tronghieudo2k4@gmail.com>
2026-01-10 16:36:42 +0700

    9. Toán tử gán

---
+++ 7/index.js
@@ -0,0 +1,3 @@
+let a = prompt("Nhập số a:");
+let b = prompt("Nhập số b:");
+console.log("Kết quả của a*b là: " + a*b);
---
+++ Toán tử gán/index.js
@@ -0,0 +1,19 @@
+let a = 7;
+a += 3;
+console.log("Giá trị của a là: " + a);
+
+let b = 15;
+b -= 6;
+console.log("Giá trị của b là: " + b);
+
+let c = 5;
+c *= 4;
+console.log("Giá trị của c là: " + c);
+
+let d = 12;
+d /= 2;
+console.log("Giá trị của d là: " + d);
+
+let e = 5;
+let f = 2;
+console.log("Giá trị của e=e-(f+1) là: " + (e = e - (f + 1)));
