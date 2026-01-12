4893710a54ed93a662cd3127b493583f5192bc71
Hiu11 <Tronghieudo2k4@gmail.com>
2026-01-12 18:29:38 +0700

    21. Do while

---
+++ 21. Do while/index.js
@@ -0,0 +1,11 @@
+// 21. do while 
+let i = 1;
+do{
+    console.log(i); 
+    i++; //i = i + 1
+}while(i<=5);
+
+// do vẫn thực hiện ít nhất một lần dù điều kiện sai
+
+// while loop kiem tra điều kiện trước khi thực hiện
+
